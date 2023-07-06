d73643100e33b4e3d6f44e71eaba14a08524645c
Xavier <xavialex7@gmail.com>
2023-07-06 21:24:58 +0200

    Endpoint processes batch of images

--- src/main.py
+++ src/main.py
@@ -1,62 +1,68 @@
+from typing import Annotated
+
 import torch
 import uvicorn
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI, UploadFile, Query, File, HTTPException
 import json
 from PIL import Image
 from transformers import YolosImageProcessor, YolosForObjectDetection
-
+from pydantic import BaseModel
 
 app = FastAPI()
+# Model initialization
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = YolosForObjectDetection.from_pretrained('hustvl/yolos-tiny', device_map='cuda:0')
 image_processor = YolosImageProcessor.from_pretrained('hustvl/yolos-tiny', device_map='cuda:0')
-classes_of_interest = [1, 3]
 
-def yolos_object_detection(img):
-    inputs = image_processor(images=img, return_tensors="pt")
+
+def yolos_object_detection(imgs, threshold, classes_of_interest):  
+    inputs = image_processor(images=imgs, return_tensors="pt")
     outputs = model(**inputs)
 
-    # model predicts bounding boxes and corresponding COCO classes
-    logits = outputs.logits
-    bboxes = outputs.pred_boxes
-
-    # print results
-    target_sizes = torch.tensor([img.size[::-1]])
-    results = image_processor.post_process_object_detection(outputs, threshold=0.9, target_sizes=target_sizes)[0]
-    idxs_of_interest = [idx for idx in range(len(results['labels'])) if idx in classes_of_interest]
-    for key, value in results.items():
-        results[key] = value[idxs_of_interest]
-    results['labels'] = [model.config.id2label[label] for label in results['labels'].tolist()]
-    #results['scores'] = torch.round(results['scores'], decimals=2)
-    output = {'labels': results['labels'], 'scores': results['scores'].tolist(), 'bboxes': results['boxes'].tolist()}
-    print(output)
+    # Results postprocessing
+    target_sizes = [img.size[::-1] for img in imgs]
+    results = image_processor.post_process_object_detection(outputs, 
+        threshold=threshold, target_sizes=target_sizes)
+    # Final result creation and selection of classes of interest
+    output = []
+    for result in results:
+        idxs_of_interest = [idx for idx in range(len(result['labels'])) 
+                            if result['labels'][idx] in classes_of_interest]
+        for key, value in result.items():
+            result[key] = value[idxs_of_interest]
+        result['labels'] = [model.config.id2label[label] 
+                            for label in result['labels'].tolist()]
+        output.append({'labels': result['labels'], 
+                       'scores': result['scores'].tolist(), 
+                       'bboxes': result['boxes'].tolist()})
+    
     return output
 
-@app.post("/uploadfile/")
-async def create_upload_file(files: list[UploadFile]):
-    print("hola")
-    img = Image.open(file.file)
-    print(img.size)
-    print(type(img))
-    output = yolos_object_detection(img)
-    #request_object_content = await file.read()
-    #image = Image.open(io.BytesIO(request_object_content))
+
+@app.post("/yolos-object-detection/")
+async def create_upload_file(
+        files: Annotated[list[UploadFile],
+            File(description="Images to be fed to the object detection model" \
+                 "Just JPEG format supported")],
+        threshold: Annotated[float, 
+            Query(description="Minimum confidence threshold for the " \
+                  "detections to be considered valid.")] = 0.9,
+        classes_of_interest: Annotated[list[int],
+            Query(title="Classes of interest", 
+                  description="IDs of the classes to be detected by the " \
+                    "object detection model: https://huggingface.co/hustvl/" \
+                    "yolos-tiny/blob/main/config.json")] = [1, 3]):
+    imgs = []
+    for file in files:
+        if file.content_type != "image/jpeg":
+            raise HTTPException(400, detail="Invalid image type." \
+                                "Just JPEG support.")
+        imgs.append(Image.open(file.file))
+    output = yolos_object_detection(imgs, threshold, classes_of_interest)
+    output = [{file.filename: result} for file, result in zip(files, output)]
+
     return output
 
-"""
-@app.post("/object-detector", status_code=202, summary="Image object detector endpoint")
-async def process_image(file: UploadFile = File(...)):
-
-    # Read the uploaded file
-    file_bytes = await file.read()
-    file_root = save_file(file_bytes=file_bytes, name=file.filename)
-
-    try:
-        results = detect_objects(detector, file_root)
-        return results
-    except Exception as err:
-        raise HTTPException(status_code=404, detail=f"Error while object detection with image {file.filename}")
-"""
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="localhost", port=5000, reload=True)
